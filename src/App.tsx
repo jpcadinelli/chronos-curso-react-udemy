438380ef0cac1038185c3ac89e376dbaa381ed2b
jpcadinelli <jpcadinelli@gmail.com>
2025-04-02 19:24:45 -0300

    Aula 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './style/theme.css'
-import './style/global.css'
+import { Heading } from './components/Heading';
+
+import './style/theme.css';
+import './style/global.css';
 
 export function App() {
     return (
         <>
-            <h1>Olá, mundo!</h1>
+            <Heading />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, soluta earum! Quidem quo odio excepturi harum, natus aut illum! Atque, amet rerum pariatur laudantium possimus dolore error molestiae adipisci nulla?</p>
         </>
     )
-}
+};
