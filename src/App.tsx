99d78a4cc003b0a1610f5ef375d48058548ed46f
jpcadinelli <jpcadinelli@gmail.com>
2025-04-05 13:27:32 -0300

    feat: aula 14

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './style/global.css';
 export function App() {
     return (
         <>
-            <Heading />
+            <Heading>Olá mundo 1!</Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, soluta earum! Quidem quo odio excepturi harum, natus aut illum! Atque, amet rerum pariatur laudantium possimus dolore error molestiae adipisci nulla?</p>
         </>
     )
