688e77305ecc16646cd69463e3862d819d4e160b
jpcadinelli <jpcadinelli@gmail.com>
2025-04-15 18:50:09 -0300

    feat: aula 42

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { Home } from './pages/Home';
+import { useState } from 'react';
+import { TaskStateModel } from './models/TaskStateModel';
 
 import './style/theme.css';
 import './style/global.css';
 
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formatedSecondsRemaining: '00:00',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    },
+};
+
 export function App() {
+    const [state, setState] = useState(initialState);
+
     return (
-        <Home />
+        <Home state={ state } setState={ setState }/>
     )
 };
